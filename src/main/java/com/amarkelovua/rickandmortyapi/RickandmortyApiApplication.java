3cb7f387571b700c5f505f555846dab0b1635ba5
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-10-25 17:21:35 +0300

    The project is created

---
+++ src/main/java/com/amarkelovua/rickandmortyapi/RickandmortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.amarkelovua.rickandmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickandmortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickandmortyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amarkelovua/rickandmortyapi/RickandmortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarkelovua.rickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickandmortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
